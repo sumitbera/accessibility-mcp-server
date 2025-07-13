4152e4db36b873f1907b9beccd51e94fbb7b80b6
Sumit Bera <sumitbera@Sumits-MacBook-Air.local>
2025-07-13 11:43:16 +1000

    add mcp client

---
+++ lambda/mcpClient.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+require ('dotenv').config();
+
+const MCP_BASE_URL = process.env.MCP_SERVER_URL || 'http://localhost:3000';
+
+async function callMCP(flow) {
+  try{
+    const res = await axios.post(`${MCP_BASE_URL}/test`, flow);
+    return res.data;
+  }catch (error) {
+    console.error('Error calling MCP server:', error.message)   ;
+    throw error;
+  }
+    
+}
+
+module.exports = { callMCP };
