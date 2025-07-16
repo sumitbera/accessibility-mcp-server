315cae690d2ca81360af1406968712d3c0d27754
Sumit Bera <sumit.bera@live.in>
2025-07-16 21:34:26 +1000

    change directory generation logic

--- utils/fsUtils.js
+++ utils/fsUtils.js
@@ -1,22 +1,23 @@
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 
 function ensureDir(dirPath) {
-    if (!fs.existsSync(dirPath)) {
-        fs.mkdirSync(dirPath, { recursive: true });
+    try {
+        fs.ensureDirSync(dirPath);
+    } catch (err) {
+        console.error(`[FS] Failed to ensure directory ${dirPath}`, err.message);
     }
 }
 
 function clearDir(dirPath) {
-    if (fs.existsSync(dirPath)) {
-        fs.readdirSync(dirPath).forEach(file => {
-            const curPath = path.join(dirPath, file);
-            fs.unlinkSync(curPath);
-        });
+    try {
+        if (fs.existsSync(dirPath)) {
+            fs.removeSync(dirPath); // Remove the entire folder
+        }
+        fs.ensureDirSync(dirPath); // Recreate after clean
+    } catch (err) {
+        console.error(`[FS] Failed to clear directory ${dirPath}`, err.message);
     }
 }
 
-module.exports = {
-    ensureDir,
-    clearDir
-}
+module.exports = { ensureDir, clearDir };
